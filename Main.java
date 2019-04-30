b56bd95a21c1322c3f8a86cb93c512c1b6bc853d
Joseph-Emmanuel <josephE.banzio@gmail.com>
2019-04-30 13:10:19 +0200

    implementation

--- Main.java
+++ Main.java
@@ -1,35 +1,12 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class Main {
 
-    public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:bdfilm";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        connect();
+        RechercheFilm uneRecherche = new RechercheFilm("bdfilm");
+
+        uneRecherche.fermeBase();
+
+        uneRecherche.retrouve("DE Hitchcock, AVEC MattPokora");
     }
 }
--- RechercheFilm.java
+++ RechercheFilm.java
@@ -1,14 +1,86 @@
+import java.lang.reflect.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RechercheFilm {
+    private Connection _connection;
+    private String[] _keywords = {"titre", "de", "avec", "pays", "en", "avant", "apres"};
 
     public RechercheFilm(String nomFichierSQLite){
+        _connection = null;
+        try {
+            String url = "jdbc:sqlite:" + nomFichierSQLite;
+            _connection = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void fermeBase(){
-
+        try {
+            _connection.close();
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public String retrouve(String requete){
+        HashMap<String, String> data = new HashMap<String, String>();
+        String word = "";
+        String actualKey= "";
+        /*
+        String[] toto = {"ta", "mère", "koko"};
+        String[] dodo = {"1", "2", "3", "4"};
+        dodo = splice(dodo, 0, 2);
+        System.out.println(Arrays.toString(dodo));
+        */
+
+        for(int i=0; i<requete.length(); i++){
+            if(requete.charAt(i)== ' '){
+                if(Arrays.asList(_keywords).contains(word)){
+                    data.put(word.toLowerCase(), null);
+                    actualKey = word;
+                }else{
+                    data.replace(actualKey, null, word.toLowerCase());
+                    actualKey="";
+                }
+                word = "";
+            }
+
+            word += requete.charAt(i);
+
+
+        }
+
+        for (String name: data.keySet()){
+            String key = name;
+            String value = data.get(name);
+            System.out.println(key + " " + value + "\n");
+        }
+
         return "";
     }
+
+
+    @SuppressWarnings("unchecked")
+    public static <T>T[] splice(final T[] array, int start, final int deleteCount) {
+        if (start < 0)
+            start += array.length;
+
+        final T[] spliced = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length - deleteCount);
+        if (start != 0)
+            System.arraycopy(array, 0, spliced, 0, start);
+
+        if (start + deleteCount != array.length)
+            System.arraycopy(array, start + deleteCount, spliced, start, array.length - start - deleteCount);
+
+        return spliced;
+    }
+
 }
